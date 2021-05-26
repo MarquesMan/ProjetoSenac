d5f6b2ec5ee515443dcd073fbf210de995e96fcf
Lucas <marquesnavarezi@hotmail.com>
2021-05-26 11:17:23 -0400

    Lag da lanterna e F para usar

---
+++ Assets/Scripts/PickupObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+
+    public AudioClip pickupSound;
+    public float pickupSpeed = 0.25f;
+    internal bool found = false;
+
+    public void PlayerPickup()
+    {
+        //if (playerTransform == null)
+        Transform playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        transform.SetParent(playerTransform, true);
+        gameObject.LeanMove(playerTransform.position + playerTransform.forward * -1, pickupSpeed); ;
+        found = true;
+        if (pickupSound) GetComponent<AudioSource>().PlayOneShot(pickupSound);
+        StartCoroutine(GoToPlayerInv());
+    }
+
+    private IEnumerator GoToPlayerInv()
+    {
+        yield return new WaitForSeconds(pickupSound ? pickupSound.length : pickupSpeed + 0.1f);
+        gameObject.SetActive(false);
+    }
+}
